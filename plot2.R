1932c962e067e6397d1cde920e2abc24a8262a65
eslam-ghazal <eslamm.gazal@gmail.com>
2017-04-28 02:17:58 +0200

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -19,7 +19,7 @@ data <- read.table(file = file.path(getwd(), "data", "household_power_consumptio
 
 data <- data[data$Date %in% c("1/2/2007","2/2/2007"),]  
 data$Date = as.Date(data$Date, format  = '%d/%m/%Y') 
-data$TimeTime = strptime(paste(data$Date, data$Time), format = '%Y-%m-%d %H:%M:%S' )
+data$Time = strptime(paste(data$Date, data$Time), format = '%Y-%m-%d %H:%M:%S' )
 
 ####
 ###
